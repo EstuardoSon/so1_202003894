1a23c8447b518d0a4ac1f23965ac32539bc4b14d
Estuardo Son <2986622050101@ingenieria.usac.edu.gt>
2023-02-13 22:25:46 -0600

    Inicio reporte frontend

--- Practica1/BashCalculadora/Script.sh
+++ Practica1/BashCalculadora/Script.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 patron=`wc -l < /app/Reporte.txt`
-echo "Logs realizados: $patron"
+echo "Logs realizados: $patron" > /tmp/consulta.log
 patron=`cat /app/Reporte.txt | grep -c Error`
-echo "Cant. Errores: $patron"
+echo "Cant. Errores: $patron" >> /tmp/consulta.log
 patron=`cat /app/Reporte.txt | grep -v Error | grep -c " + "`
-echo "Cant. Suma: $patron"
+echo "Cant. Suma: $patron" >> /tmp/consulta.log
 patron=`cat /app/Reporte.txt | grep -v Error | grep -c " - "`
-echo "Cant. Resta: $patron"
+echo "Cant. Resta: $patron" >> /tmp/consulta.log
 patron=`cat /app/Reporte.txt| grep -v Error | grep -c " \* "`
-echo "Cant. Mult: $patron"
+echo "Cant. Mult: $patron" >> /tmp/consulta.log
 patron=`cat /app/Reporte.txt| grep -v Error | grep -c " / "`
-echo "Cant. Div: $patron"
+echo "Cant. Div: $patron" >> /tmp/consulta.log
 fecha=`date +%F`
 patron=`cat /app/Reporte.txt | grep "$fecha"`
-echo "\nLogs de $fecha \n$patron"
+echo "\nLogs de $fecha \n$patron" >> /tmp/consulta.log
+cat /tmp/consulta.log
